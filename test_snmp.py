e6d523123b7a84bb002aecaf2cd5e3c050485f5d
rootbmb <rootbmb@gmail.com>
2023-09-09 14:32:35 +0300

    fix

--- test_snmp.py
+++ test_snmp.py
@@ -2,7 +2,7 @@ import asyncio
 import aiosnmp
 
 async def main():
-    async with aiosnmp.Snmp(host='10.10.8.144', port=161, community="public", timeout=1) as snmp:
+    async with aiosnmp.Snmp(host='10.10.5.131', port=161, community='public', timeout=1) as snmp:
         for res in await snmp.get('.1.3.6.1.2.1.1.1.0'):
             print(res.oid, res.value)
 
